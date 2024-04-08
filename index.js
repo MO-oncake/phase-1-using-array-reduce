9dbe7b2a59f352c64c7de819bafe7d93d2c0618b
William Gitau <dontysh@gmail.com>
2024-04-08 20:41:12 +0300

    employes the reduce() function to find the sum of the batteries in the array.

--- index.js
+++ index.js
@@ -1,3 +1,7 @@
 const batteryBatches = [4, 5, 3, 4, 4, 6, 5];
 
-// Code your solution here
+const totalBatteries = batteryBatches.reduce((accumulator, currentValue) => accumulator + currentValue, 0);
+
+// The variable totalBatteries takes the batteryBatches array and by using reduce() it adds up all the values in the array. 
+// The reduce() method takes two arguments: the accumulator and the current value. The accumulator is the variable that will hold the total, and the current value is the value of the current item in the array. 
+// The accumulator is initialized to 0 as it's starting value. This value could be altered at any point in the reduce() method depending on the number of batteries on hand at the time.
